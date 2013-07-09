513c9eb3077645faac7bbb285a5b5644b812fd62
wjch111 <wjch111@gmail.com>
2013-07-09 17:16:10 +0200

    July 9 modif

---
+++ PTSP2013Wang/src/controllers/momcts/ExpController.java
@@ -0,0 +1,22 @@
+package controllers.momcts;
+
+import java.util.Random;
+
+import controllers.utils.DebugTools;
+
+import framework.core.Controller;
+import framework.core.Game;
+
+public class ExpController extends Controller {
+	int i;
+	
+	public ExpController(Game a_game, long a_timeDue){
+		i=0;
+	}
+	
+	public int getAction(Game a_game, long a_timeDue) {
+		i++;
+		return 3+i%6;
+	}
+
+}
